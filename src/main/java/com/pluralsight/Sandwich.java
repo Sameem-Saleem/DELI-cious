00f1e98674db73d190bde38313a52b66f957443f
Sameem <sameem@berkeley.edu>
2024-11-15 04:17:02 -0800

    Fix Sandwich Class - add toasted

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -7,36 +7,29 @@ public class Sandwich {
     private final int inches;
     private final Boolean extraMeat;
     private final Boolean extraCheese;
+    private final Boolean toasted;
     private final ArrayList<String> toppings;
 
-    public Sandwich(String bread, int inches, ArrayList<String> toppings, Boolean extraMeat, Boolean extraCheese) {
+    public Sandwich(String bread, int inches, ArrayList<String> toppings, Boolean extraMeat, Boolean extraCheese, Boolean toasted) {
         this.bread = bread;
         this.inches = inches;
         this.toppings = toppings;
         this.extraMeat = extraMeat;
         this.extraCheese = extraCheese;
+        this.toasted = toasted;
     }
 
     public double getPrice() {
-        double totalPrice = 0;
+        double totalPrice = this.inches == 4 ? 5.50 : this.inches == 8 ? 7.00 : this.inches == 12 ? 8.50 : 0;
         double toppingMultiplier = (double) this.inches / 4;
-        switch (this.inches) {
-            case 4 -> totalPrice += 5.50;
-            case 8 -> totalPrice += 7.00;
-            case 12 -> totalPrice += 8.50;
-        }
         for (String topping : this.toppings) {
             switch (topping) {
                 case "steak", "ham", "salami", "roast beef", "chicken", "bacon" -> totalPrice += toppingMultiplier;
                 case "american", "provolone", "cheddar", "swiss" -> totalPrice += 0.75 * toppingMultiplier;
             }
         }
-        if (this.extraCheese) {
-            totalPrice += 0.30 * toppingMultiplier;
-        }
-        if (this.extraMeat) {
-            totalPrice += 0.50 * toppingMultiplier;
-        }
-        return totalPrice;
+        totalPrice += this.extraCheese ? 0.30 * toppingMultiplier : 0;
+        totalPrice += this.extraMeat ? 0.50 * toppingMultiplier : 0;
+        return this.toasted ? totalPrice * 1.15 : totalPrice;
     }
 }
